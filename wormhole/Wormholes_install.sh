02e8ad9124e87d8cece3de2de81611ef9f5241ca
meowment <65986100+meowment@users.noreply.github.com>
2022-10-03 17:11:06 +0700

    Update Wormholes_install.sh

--- wormhole/Wormholes_install.sh
+++ wormhole/Wormholes_install.sh
@@ -8,6 +8,20 @@ echo -e "\e[0m"
 
 sleep 2
 
+echo -e "\e[1m\e[32m1. Updating dependencies... \e[0m" && sleep 1
+sudo apt update && sudo apt upgrade -y
+
+echo -e "\e[1m\e[32m3. Checking if Docker is installed... \e[0m" && sleep 1
+if ! command -v docker &> /dev/null
+then
+    echo -e "\e[1m\e[32m3.1 Installing Docker... \e[0m" && sleep 1
+    sudo apt-get install ca-certificates curl gnupg lsb-release -y
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+fi
+
 #check docker cmd
 which docker >/dev/null 2>&1
 if  [ $? -ne 0 ] ; then
