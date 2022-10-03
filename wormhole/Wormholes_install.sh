2f52a360f007cd14a4ed33e3defba1e543ebf004
meowment <65986100+meowment@users.noreply.github.com>
2022-10-03 17:18:10 +0700

    Update Wormholes_install.sh

--- wormhole/Wormholes_install.sh
+++ wormhole/Wormholes_install.sh
@@ -8,19 +8,23 @@ echo -e "\e[0m"
 
 sleep 2
 
-echo -e "\e[1m\e[32m1. Updating dependencies... \e[0m" && sleep 1
+#Update packages
 sudo apt update && sudo apt upgrade -y
 
-echo -e "\e[1m\e[32m3. Checking if Docker is installed... \e[0m" && sleep 1
-if ! command -v docker &> /dev/null
-then
-    echo -e "\e[1m\e[32m3.1 Installing Docker... \e[0m" && sleep 1
-    sudo apt-get install ca-certificates curl gnupg lsb-release -y
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get update
-    sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-fi
+#Install dependencies
+sudo apt install wget jq build-essential nano unzip -y
+
+#Install docker
+sudo apt-get install ca-certificates curl gnupg lsb-release -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
+#Install docker compose
+docker_compose_version=$(wget -qO- https://api.github.com/repos/docker/compose/releases/latest | jq -r ".tag_name")
+sudo wget -O /usr/bin/docker-compose "https://github.com/docker/compose/releases/download/${docker_compose_version}/docker-compose-`uname -s`-`uname -m`"
+sudo chmod +x /usr/bin/docker-compose
 
 #check docker cmd
 which docker >/dev/null 2>&1
