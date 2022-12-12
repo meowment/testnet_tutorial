7ff10a78864780814b221378e27427512c044dbe
meowment <65986100+meowment@users.noreply.github.com>
2022-12-12 10:46:42 +0700

    Update defund.sh

--- defund/defund.sh
+++ defund/defund.sh
@@ -94,9 +94,6 @@ sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0ufetf\"/" $HOME/.d
 # enable prometheus
 sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.defund/config/config.toml
 
-# reset
-defundd tendermint unsafe-reset-all --home $HOME/.defund
-
 echo -e "\e[1m\e[32m4. Starting service... \e[0m" && sleep 1
 # create service
 sudo tee /etc/systemd/system/defundd.service > /dev/null <<EOF
